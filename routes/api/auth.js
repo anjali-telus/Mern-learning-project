25d0e0029c11f199cf84f6cfce019506a68bba23
Anjali Verma <anjali.verma@telusinternational.com>
2021-08-02 22:01:46 +1200

    implementation of JWT

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+
+// router.get('/', auth, (req,res) => res.send('Auth route'));
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
+//@route     POST api/auth
+//@desc      Authenticate user and get token 
+//@access    public
+
+//see if user exists
+router.post('/', [
+  // check('name', 'Name is required')
+  //     .not().isEmpty(),
+  check('email', 'Please enter a valid email').isEmail(),
+  check('password', 'please enter a valid password with atleast 6 character').exists()
+],
+  async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() })
+      }
+
+      const { email, password } = req.body;
+      try {
+          //see if user exists
+          let user = await User.findOne({ email });
+
+          if (!user) {
+              return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+          }
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+          return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] });
+        }
+          const payload = {
+             user:{
+               id:user.id
+              } 
+          };
+
+          jwt.sign(
+             payload, config.get('jwtSecret'),
+          { expiresIn:36000 },
+          (err,token) => {          //callback
+              if (err) throw err;
+              res.json({ token });
+          }
+          );
+
+      } catch (err) {
+          console.error(err.message);
+          res.status(500).send('server error');
+
+      }
 
 
-router.get('/',(req,res) => res.send('Auth route'));
-
-module.exports= router;
+  });
+module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 //@route     GET api/users
@@ -74,9 +75,24 @@ router.post('/', [
             user.password = await bcrypt.hash(password, salt);
             await user.save();
 
-            res.send('users registered');
-        }
-        catch (err) {
+            // res.send('users registered');
+
+            const payload = {
+               user:{
+                 id:user.id
+                } 
+            };
+
+            jwt.sign(
+               payload, config.get('jwtSecret'),
+            { expiresIn:36000 },
+            (err,token) => {          //callback
+                if (err) throw err;
+                res.json({ token });
+            }
+            );
+
+        } catch (err) {
             console.error(err.message);
             res.status(500).send('server error');
 
